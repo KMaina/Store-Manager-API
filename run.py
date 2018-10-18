d75b6fb4505b70c8b5dc3df35fe288248f92c0f5
KMaina <maina.ken0@gmail.com>
2018-10-18 23:43:30 +0300

    [Chore #161243946] Add a Procfile for hosting on Heroku

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ from app import create_app
 config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
 
+posrt = os.environ.get("PORT", 5000)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=port)
 
