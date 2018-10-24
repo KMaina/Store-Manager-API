443cbfd2a2a220dcdc8ac4db4a876ec6e826b7b9
KMaina <maina.ken0@gmail.com>
2018-10-24 22:24:59 +0300

    [Feature #161399031] Refactor the tests to make them pass

--- app/tests/v2/test_views_products_v2.py
+++ app/tests/v2/test_views_products_v2.py
@@ -32,6 +32,12 @@ class UserTestCase(unittest.TestCase):
             "product_cost":"",
             "reorder":20
         }
+        self.product4 = {
+            "name":"eggs",
+            "quantity":30,
+            "product_cost":-1,
+            "reorder":20
+        }
     
         with self.app.app_context():
             db.db_connection()
@@ -46,43 +52,53 @@ class UserTestCase(unittest.TestCase):
 
     def test_add_new_product(self):
         """Test to successfuly add a new product in the database"""
-        response = self.client().post('/api/v2/auth/login', data=json.dumps({self.admin}), content_type='application/json')
+        response = self.client().post('/api/v2/auth/login', data=json.dumps(self.admin), content_type='application/json')
         json_data = json.loads(response.data)
         access_token = json_data.get('access_token')
         self.assertEqual(response.status_code, 200)
-        response = self.client().post('/api/v2/auth/signup', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product1), content_type='application/json')
+        response = self.client().post('/api/v2/products', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product1), content_type='application/json')
         self.assertEqual(response.status_code, 201)
         self.assertIn('Product Successfully Created', str(response.data))
     
     def test_product_already_exists(self):
         """Test to successfuly add a new product in the database"""
-        response = self.client().post('/api/v2/auth/login', data=json.dumps({self.admin}), content_type='application/json')
+        response = self.client().post('/api/v2/auth/login', data=json.dumps(self.admin), content_type='application/json')
         json_data = json.loads(response.data)
         access_token = json_data.get('access_token')
         self.assertEqual(response.status_code, 200)
-        response = self.client().post('/api/v2/auth/signup', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product1), content_type='application/json')
+        response = self.client().post('/api/v2/products', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product1), content_type='application/json')
         self.assertEqual(response.status_code, 201)
         self.assertIn('Product Successfully Created', str(response.data))
-        response = self.client().post('/api/v2/auth/signup', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product1), content_type='application/json')
+        response = self.client().post('/api/v2/products', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product1), content_type='application/json')
         self.assertEqual(response.status_code, 401)
         self.assertIn('Product already exists', str(response.data))
     
     def test_product_missing_values(self):
         """Test to handle missing values"""
-        response = self.client().post('/api/v2/auth/login', data=json.dumps({self.admin}), content_type='application/json')
+        response = self.client().post('/api/v2/auth/login', data=json.dumps(self.admin), content_type='application/json')
         json_data = json.loads(response.data)
         access_token = json_data.get('access_token')
         self.assertEqual(response.status_code, 200)
-        response = self.client().post('/api/v2/auth/signup', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product2), content_type='application/json')
+        response = self.client().post('/api/v2/products', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product2), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertIn('You must supply the cost of the product', str(response.data))
     
     def test_product_empty_values(self):
         """Test to handle empty values"""
-        response = self.client().post('/api/v2/auth/login', data=json.dumps({self.admin}), content_type='application/json')
+        response = self.client().post('/api/v2/auth/login', data=json.dumps(self.admin), content_type='application/json')
         json_data = json.loads(response.data)
         access_token = json_data.get('access_token')
         self.assertEqual(response.status_code, 200)
-        response = self.client().post('/api/v2/auth/signup', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product3), content_type='application/json')
+        response = self.client().post('/api/v2/products', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product3), content_type='application/json')
         self.assertEqual(response.status_code, 401)
         self.assertIn('Fields cannot be empty', str(response.data))
+    
+    def test_product_negative_values(self):
+        """Test to handle if values are less than 1"""
+        response = self.client().post('/api/v2/auth/login', data=json.dumps(self.admin), content_type='application/json')
+        json_data = json.loads(response.data)
+        access_token = json_data.get('access_token')
+        self.assertEqual(response.status_code, 200)
+        response = self.client().post('/api/v2/products', headers = {"Authorization":"Bearer " + access_token}, data=json.dumps(self.product4), content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+        self.assertIn('Values cannot be less than 1', str(response.data))
