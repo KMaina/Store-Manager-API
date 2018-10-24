760acc409e2c10a42887f9c6d4fb18089152107d
KMaina <maina.ken0@gmail.com>
2018-10-24 17:51:49 +0300

    [Feature #161396788] Add a resource for signing up users

--- app/api/v2/resource/views/views_users.py
+++ app/api/v2/resource/views/views_users.py
@@ -6,7 +6,7 @@ from app.api.v2.resource.models.model_users import Users
 parser = reqparse.RequestParser(bundle_errors=True)
 parser.add_argument('name', help="You must supply a username", required='True')
 parser.add_argument('password', help="You must supply a password", required='True')
-# parser.add_argument('confirm', help="You must supply a confirmation for your password", required='True')
+parser.add_argument('confirm', help="You must supply a confirmation for your password", required='True')
 
 class LoginUsers(Resource):
     """
@@ -19,3 +19,16 @@ class LoginUsers(Resource):
         return Users().login_user(
             args['name'],
             args['password'])
+
+class RegisterUsers(Resource):
+    """
+    Class to handle adding users
+    POST /api/v2/auth/signup -> Creates a new user
+    """
+    def post(self):
+        """Route to handle creating users"""
+        args = parser.parse_args()
+        return Users().reg_user(
+            args['name'],
+            args['password'],
+            args['confirm'])
