e2f4bfd3f58055bad2d3923973aa27c7b29d9fed
KMaina <maina.ken0@gmail.com>
2018-10-24 15:27:56 +0300

    [Feature #161396950] Refactor tests to make them pass

--- app/tests/v2/test_views_users_v2.py
+++ app/tests/v2/test_views_users_v2.py
@@ -12,52 +12,51 @@ class UserTestCase(unittest.TestCase):
         self.client = self.app.test_client
     
         with self.app.app_context():
-            db.main_test()
-            
+            db.db_connection()
+            db.create_tables()
+            db.generate_admin()
+        
+    def tearDown(self):
+        """Drops all tables after tests are done"""
+        with self.app.app_context():
+            db.db_connection()
+            db.drop_tables()
     
     def test_user_login(self):
         """Successfully log into the app"""
-        response = self.client().post('/api/v2/auth/signup', data=json.dumps({
+        response = self.client().post('/api/v2/auth/login', data=json.dumps({
             "name":"admin",
             "password":"passadmin"
         }), content_type='application/json')
-        connection = db.db_connection_test()
-        cursor = connection.cursor()
-
+        
         self.assertEqual(response.status_code, 200)
-        self.assertIn('User Successfully Logged In', str(response.data))
+        self.assertIn('User Successfully logged in', str(response.data))
 
     def test_no_user_in_db(self):
         """Test if the user is not in the database"""
-        response = self.client().post('/api/v2/auth/signup', data=json.dumps({
+        response = self.client().post('/api/v2/auth/login', data=json.dumps({
             "name":"ken",
             "password":"mypass"
         }), content_type='application/json')
-        connection = db.db_connection_test()
-        cursor = connection.cursor()
 
         self.assertEqual(response.status_code, 401)
-        self.assertIn('Sorry, credentials not found', str(response.data)) 
+        self.assertIn('Error logging in, credentials not found', str(response.data)) 
     
     def test_empty_credentials(self):
         """Test for empty values"""
-        response = self.client().post('/api/v2/auth/signup', data=json.dumps({
+        response = self.client().post('/api/v2/auth/login', data=json.dumps({
             "name":"",
             "password":"mypass"
         }), content_type='application/json')
-        connection = db.db_connection_test()
-        cursor = connection.cursor()
 
         self.assertEqual(response.status_code, 401)
         self.assertIn('Fields cannot be empty', str(response.data))
     
     def test_missing_credentials(self):
         """Test for missing parameters"""
-        response = self.client().post('/api/v2/auth/signup', data=json.dumps({
+        response = self.client().post('/api/v2/auth/login', data=json.dumps({
             "password":"mypass"
         }), content_type='application/json')
-        connection = db.db_connection_test()
-        cursor = connection.cursor()
 
-        self.assertEqual(response.status_code, 401)
-        self.assertIn('You must supply a username', str(response.data)) 
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('You must supply a username', str(response.data))
