b695cb7481ca14974a85ca9eabac41df08d0061a
KMaina <maina.ken0@gmail.com>
2018-10-26 01:57:57 +0300

    [Fix] fix error in Heroku

--- instance/config.py
+++ instance/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     """Parent configuration class."""
     SECRET_KEY = os.getenv('SECRET_KEY')
-    DB_PATH = os.getenv('DB_DEV')
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
@@ -14,7 +14,7 @@ class TestingConfig(Config):
     """Configurations for Testing"""
     TESTING = True
     DEBUG = True
-    DB_PATH = os.getenv('DB_TESTING')
+    DATABASE_URL = os.getenv('DB_TESTING')
 
 class StagingConfig(Config):
     """Configurations for Staging."""
