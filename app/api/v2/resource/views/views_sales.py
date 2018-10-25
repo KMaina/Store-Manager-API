ec5c296aff2e27129f61e82c81a047741e731c23
KMaina <maina.ken0@gmail.com>
2018-10-25 15:11:34 +0300

    [Feature #161400199] Add authentication for the endpoint

--- app/api/v2/resource/views/views_sales.py
+++ app/api/v2/resource/views/views_sales.py
@@ -1,6 +1,7 @@
 """Views for the Sales Resource"""
 from flask_restful import Resource, reqparse
 from flask import request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 parser = reqparse.RequestParser(bundle_errors=True)
 parser.add_argument('name', help="You must supply a product name", required='True')
@@ -15,6 +16,10 @@ class MakeSales(Resource):
     def post(self):
         """Route to handle creating sales"""
         args = parser.parse_args()
+
+        current_user = get_jwt_identity()
+        if current_user['admin'] == True:
+            return {'msg':'Sorry, this route is only accessible to sales attendants'}, 403
         return Sales().add_sale(
             args['name'],
             args['quantity'])
