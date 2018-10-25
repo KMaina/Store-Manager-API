19a8a2e9128ce2e93ae7733a73ace91d7aeac9ac
KMaina <maina.ken0@gmail.com>
2018-10-26 00:22:05 +0300

    [Feature #161400199] Code cleanup

--- app/api/v2/resource/models/model_sales.py
+++ app/api/v2/resource/models/model_sales.py
@@ -1,8 +1,8 @@
 """handles all CRUD operations for the sales endpoint"""
 import psycopg2
-from flask import request, jsonify
+from flask import request, jsonify, current_app
 from flask_jwt_extended import get_jwt_identity
-from flask import current_app
+
 from app.api.v2.resource.models import db
 
 class Sales:
@@ -26,25 +26,29 @@ class Sales:
         # Checks for values less than 1
         if quantity < 1:
             return {'msg':'Values cannot be less than 1'}, 401
-        
+
         # Get the quantity added
         get_quantity = db.get_quantity_of_products(name)
         if quantity > get_quantity:
             return {'msg':'Quantity order is more than that in stock'}, 401
         new_quantity = get_quantity - quantity
-        
+
         get_price = db.get_price_of_products(name)
         total_cost = get_price * quantity
 
-        id = db.get_product_id('products', 'product_name', name)
+        product_id = db.get_product_id('products', 'product_name', name)
 
         try:
-            modify_product = """UPDATE products 
-                                SET quantity = {}
-                                WHERE product_name = '{}'""".format(new_quantity, name)
-            new_sale = "INSERT INTO \
-                          sales (product_id, totat_cost, user_id) \
-                          VALUES ('{}','{}',{})".format(id, total_cost, current_user['id'])               
+            modify_product = """
+                            UPDATE products
+                            SET quantity = {}
+                            WHERE product_name = '{}'
+                            """.format(new_quantity, name)
+            new_sale = """
+                        INSERT INTO 
+                        sales (product_id, totat_cost, user_id) 
+                        VALUES ('{}','{}',{})
+                        """.format(product_id, total_cost, current_user['id'])
             connection = db.db_connection()
             cursor = connection.cursor()
             cursor.execute(modify_product)
@@ -59,5 +63,3 @@ class Sales:
             response = jsonify({'msg':'Problem inserting record into the database'})
             response.status_code = 400
             return response
-        
-
